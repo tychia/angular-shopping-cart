708cd6541f93d1d69b940acd49dfcb84062f015b
tychia <yeechia@gmail.com>
2022-04-20 11:10:38 +0800

    Service Cart + Form

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Product } from './products';
 
 @Injectable({
@@ -19,5 +20,12 @@ export class CartService {
     this.items = [];
     return this.items;
   }
-  constructor() {}
+
+  getShippingPrices() {
+    return this.http.get<{ type: string; price: number }[]>(
+      'assets/shipping.json'
+    );
+  }
+
+  constructor(private http: HttpClient) {}
 }
